5cab82148314659a9c1ce9235d7137e62600fb69
Arunmozhi <arun@arunmozhi.in>
2015-08-30 10:27:21 +0530

    test metadata

--- oxdpython/__init__.py
+++ oxdpython/__init__.py
@@ -1,1 +1,5 @@
 from client import Client
+
+__description__ = "A Python Client for oxD Server"
+__version__ = "0.0.1"
+__author__ = "Gluu"
--- tests/test_package.py
+++ tests/test_package.py
@@ -1,4 +1,9 @@
-from oxdpython import Client
+import oxdpython
 
-def test_initialization():
-    assert Client
+from nose.tools import assert_equal
+
+def test_metadata():
+    assert_equal(oxdpython.__name__, "oxdpython")
+    assert_equal(oxdpython.__description__, "A Python Client for oxD Server")
+    assert_equal(oxdpython.__version__, "0.0.1")
+    assert_equal(oxdpython.__author__, "Gluu")
