ee8f23bb1a88186c78daede260ab5591074a3cd9
Arunmozhi <arun@arunmozhi.in>
2015-09-15 23:26:23 +0530

    added get function to the configurer

--- oxdpython/configurer.py
+++ oxdpython/configurer.py
@@ -1,6 +1,6 @@
 import os
 
-from ConfigParser import SafeConfigParser
+from ConfigParser import SafeConfigParser, NoOptionError, NoSectionError
 
 
 class Configurer(object):
@@ -17,3 +17,26 @@ class Configurer(object):
                 self.parser.write(cfile)
 
         self.parser.read(self.client_file)
+
+    def get(self, section, key):
+        """get function reads the config value for the requested section and
+        key and returns it
+
+        Args:
+            section (string) - the section to look for the config value
+                               either - oxd, client, provider
+            key (string) - the key for the config value required
+
+        Returns:
+            value (multiple) - the function returns the value of the key
+                               in the appropriate format if found or returns
+                               None if such a section or key couldnot be found
+
+        Example:
+            config = Configurer(location)
+            oxd_port = config.get('oxd', 'port')  # returns the port of the oxd
+        """
+        try:
+            return self.parser.get(section, key)
+        except (NoOptionError, NoSectionError):
+            return None
--- tests/test_configurer.py
+++ tests/test_configurer.py
@@ -1,10 +1,21 @@
 import os.path
-from nose.tools import assert_true
+from nose.tools import assert_true, assert_equal, assert_is_none
 
 from oxdpython.configurer import Configurer
 
+location = os.path.dirname(os.path.realpath(__file__))
+
 
 def test_oxdconfig_looks_for_config_file_while_initialization():
-    location = os.path.dirname(os.path.realpath(__file__))
     config = Configurer(location)
     assert_true(os.path.isfile(config.client_file))
+
+
+def test_get_function_returns_value_for_set_config_value():
+    config = Configurer(location)
+    # for a set value it should be the set value
+    assert_equal(config.get('oxd', 'port'), '8099')
+
+    # for an unset value it should be none
+    assert_is_none(config.get('oxd', 'message'))
+    assert_is_none(config.get('two', 'coffee'))
