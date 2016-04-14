10a7accb099b0468097dcbea4d70c92c635f5d19
Arunmozhi <arun@arunmozhi.in>
2016-04-14 20:12:41 +0530

    updated docs to make docs guidelines and rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ author = u'Gluu'
 # built documents.
 #
 # The short X.Y version.
-version = u'2.4'
+version = u'2.4.2'
 # The full version, including alpha/beta/rc tags.
 release = u'2.4.2'
 
@@ -113,7 +113,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
