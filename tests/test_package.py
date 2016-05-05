36e5a43e32e9bbfc45bb147e52ae5fe1c6d0547b
Arunmozhi <arun@arunmozhi.in>
2016-05-05 15:39:16 +0530

    updated version in unittest

--- tests/test_package.py
+++ tests/test_package.py
@@ -5,5 +5,5 @@ from nose.tools import assert_equal
 def test_metadata():
     assert_equal(oxdpython.__name__, "oxdpython")
     assert_equal(oxdpython.__description__, "A Python Client for oxD Server")
-    assert_equal(oxdpython.__version__, "0.0.1")
+    assert_equal(oxdpython.__version__, "2.4.3-master")
     assert_equal(oxdpython.__author__, "Gluu")
